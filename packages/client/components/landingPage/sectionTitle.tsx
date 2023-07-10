f8fe139b2ce453b7d8aca7eac5cc51a65db7a1e3
Amit Amanna <amit.natheniel@gmail.com>
2023-07-10 19:16:12 +0200

    animate lines on sectionTitle with useOnScreen hook

--- packages/client/components/landingPage/sectionTitle.tsx
+++ packages/client/components/landingPage/sectionTitle.tsx
@@ -1,6 +1,7 @@
 import styles from '@components/landingPage/sectionTitle.module.scss'
 import Box from '@mui/material/Box'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useOnScreen from '@utils/useOnScreen'
 
 interface SectionTitleProps {
   title: string
@@ -12,11 +13,27 @@ const SectionTitle = ({
   title,
   description = '',
   className = '',
-}: SectionTitleProps) => (
-  <Box className={`${styles.container} ${className}`}>
-    <h1 className={styles.title}>{title}</h1>
-    <p className={styles.description}>{description}</p>
-  </Box>
-)
+}: SectionTitleProps) => {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [everShown, setEverShown] = useState(false)
+  const visible = useOnScreen(ref)
+
+  useEffect(() => {
+    if (visible) {
+      setEverShown(true)
+    }
+  }, [visible])
+
+  const visibleClassName = everShown ? styles.visible : ''
+
+  return (
+    <Box className={`${styles.container} ${className}`}>
+      <h1 ref={ref} className={`${styles.title} ${visibleClassName}`}>
+        {title}
+      </h1>
+      <p className={styles.description}>{description}</p>
+    </Box>
+  )
+}
 
 export default SectionTitle
