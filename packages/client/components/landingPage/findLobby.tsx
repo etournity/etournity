b513d08449ddfd106987edccbb0e30da8825778d
Amit Amanna <amit.natheniel@gmail.com>
2023-07-11 19:28:11 +0200

    add animations to findLobby section of landing page

--- packages/client/components/landingPage/findLobby.tsx
+++ packages/client/components/landingPage/findLobby.tsx
@@ -2,26 +2,41 @@ import React from 'react'
 import styles from './findLobby.module.scss'
 import Box from '@mui/material/Box'
 import SectionTitle from '@components/landingPage/sectionTitle'
+import useOnScreen from '@utils/useOnScreen'
 
-const FindLobby = () => (
-  <Box className={styles.findLobby}>
-    <SectionTitle
-      title="Play games with the community"
-      description="With all these tools at your disposal, we need to make sure that you know what's happening at every step of the way when your using Etournity."
-    />
-    <Box className={styles.imageContainer}>
-      <img
-        className={`${styles.image} ${styles.imageLg}`}
-        src="/assets/landingpage/findLobbyLg.png"
-        alt="Find Lobby"
-      />
-      <img
-        className={`${styles.image} ${styles.imageSm}`}
-        src="/assets/landingpage/findLobbySm.png"
-        alt="Find Lobby"
-      />
-    </Box>
-  </Box>
+const Img = ({ name }: { name: string }) => (
+  <img
+    className={`${styles.image} ${styles[name]}`}
+    src={`/assets/landingpage/findLobby${name}.svg`}
+    alt={name}
+  />
 )
+
+const FindLobby = () => {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const { everShown } = useOnScreen(ref)
+  const visibleClassName = everShown ? styles.visible : ''
+
+  return (
+    <Box className={styles.findLobby}>
+      <SectionTitle
+        title="Play games with the community"
+        description="With all these tools at your disposal, we need to make sure that you know what's happening at every step of the way when your using Etournity."
+      />
+      <Box className={styles.SearchWrapper}>
+        <Img name="SearchLg" />
+        <Img name="SearchSm" />
+      </Box>
+      <Box ref={ref} className={`${styles.bottom} ${visibleClassName}`}>
+        <Img name="Filters" />
+        <Box className={styles.CardWrapper}>
+          <Img name="Card1" />
+          <Img name="Card2" />
+          <Img name="Card3" />
+        </Box>
+      </Box>
+    </Box>
+  )
+}
 
 export default FindLobby
