a2a50e552f82ebbc2b4858f962021f544fd8287c
Amit Amanna <amit.natheniel@gmail.com>
2023-08-29 19:38:57 +0200

    remove tetris game from final landingpage section

--- packages/client/components/landingPage/tetris.tsx
+++ packages/client/components/landingPage/tetris.tsx
@@ -1,37 +1,14 @@
 import React, { useEffect } from 'react'
-import { Unity, useUnityContext } from 'react-unity-webgl'
 import styles from './tetris.module.scss'
 import Box from '@mui/material/Box'
-import useOnScreen from '@utils/useOnScreen'
 import Button from '@components/landingPage/button'
 import { useRouter } from 'next/router'
 
 const Tetris = () => {
   const router = useRouter()
 
-  const { unityProvider, sendMessage } = useUnityContext({
-    loaderUrl: '/assets/landingpage/tetris/Build/tetris.loader.js',
-    dataUrl: '/assets/landingpage/tetris/Build/tetris.data',
-    frameworkUrl: '/assets/landingpage/tetris/Build/tetris.framework.js',
-    codeUrl: '/assets/landingpage/tetris/Build/tetris.wasm',
-  })
-
-  const ref = React.useRef<HTMLDivElement>(null)
-  const { isIntersecting } = useOnScreen(ref)
-
-  useEffect(() => {
-    if (isIntersecting) {
-      sendMessage('Board', 'ResumeGame')
-    } else {
-      sendMessage('Board', 'PauseGame')
-    }
-  }, [isIntersecting, sendMessage])
-
   return (
-    <Box className={styles.component} data-cy="tetris">
-      <Box ref={ref}>
-        <Unity unityProvider={unityProvider} className={styles.tetris} />
-      </Box>
+    <Box className={styles.tetris} data-cy="tetris">
       <Box className={styles.content}>
         <h1 className={styles.title}> JUMP RIGHT INTO IT.</h1>
         <Box className={styles.buttonWrapper}>
@@ -51,6 +28,22 @@ const Tetris = () => {
           </Button>
         </Box>
       </Box>
+      <img
+        className={styles.tetrisTiles}
+        src="/assets/landingpage/tetris1.svg"
+      />
+      <img
+        className={styles.tetrisTiles}
+        src="/assets/landingpage/tetris2.svg"
+      />
+      <img
+        className={styles.tetrisTiles}
+        src="/assets/landingpage/tetris3.svg"
+      />
+      <img
+        className={styles.tetrisTiles}
+        src="/assets/landingpage/tetris4.svg"
+      />
     </Box>
   )
 }
